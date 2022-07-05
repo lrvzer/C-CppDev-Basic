f3491a1fbf7be94c197f15dc1ee4bba54e8eca0f
Lrwei <a2413044193@gmail.com>
2022-07-05 17:29:57 +0800

    数据类型强化

--- CDeep/Chapter1_DataType/DataType_4_Unsigned.c
+++ CDeep/Chapter1_DataType/DataType_4_Unsigned.c
@@ -6,9 +6,14 @@ a + b = 4294967197
 #endif
 
 // 隐式类型转化
+// unsigned int + int 会先把 int 转为 unsigned int
 int main(int argc, char *argv[]) {
     unsigned int a = 1;
     int b = -100;
+
+    printf("%d, %u\n", a, a);
+    printf("%d, %u\n", b, b);
+
     printf("a + b = %d\n", a + b);
     printf("a + b = %u\n", a + b);
 
